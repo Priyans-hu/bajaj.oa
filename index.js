da799e867cc4120b8624296ece683da5ad5a47b7
Priyanshu Garg <92843561+Priyans-hu@users.noreply.github.com>
2024-03-18 07:27:10 +0000

    code updated

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ app.use(bodyParser.json());
 
 app.post('/bfhl', (req, res) => {
     try {
-        const { array } = req.body;
-        if (!array || !Array.isArray(array)) {
-            throw new Error('Invalid input. Expecting an array.');
+        const { data } = req.body;
+        if (!data || !Array.isArray(data)) {
+            throw new Error('Invalid input. Expecting an data.');
         }
 
-        const evenNumbers = array.filter(num => num % 2 === 0);
-        const oddNumbers = array.filter(num => num % 2 !== 0);
-        const alphabets = array.filter(char => /[a-zA-Z]/.test(char)).map(char => char.toUpperCase());
+        const evenNumbers = data.filter(num => num % 2 === 0);
+        const oddNumbers = data.filter(num => num % 2 !== 0);
+        const alphabets = data.filter(char => /[a-zA-Z]/.test(char)).map(char => char.toUpperCase());
 
         const user_id = 'priyanshu_garg_2065';
         const email_id = 'priyanshu2065.be21@chitkara.edu.in';
